a88631c98661d52da1436658283324984fe3f1ab
RokFonovic <rok.fonovic@student.um.si>
2025-06-01 11:39:12 +0200

    Propravljeno za streznik

--- apps/webapp1_realtime_data/src/api.ts
+++ apps/webapp1_realtime_data/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:  "http://localhost:3000/api", //import.meta.env.VITE_API_BASE_URL,//"https://or-ecosystem.eu/api",  <--- CHANGE TO SECOND OPTION FOR PRODUCTION
+  baseURL: import.meta.env.VITE_API_BASE_URL,//"https://or-ecosystem.eu/api", "http://localhost:3000/api",
   withCredentials: true,
 });
 
