aaacdfe08a890017f0aba72a7d28c9f5ebe0a11a
carluka <carluka33@gmail.com>
2025-06-03 22:03:05 +0200

    loading

--- apps/webapp1_realtime_data/src/api.ts
+++ apps/webapp1_realtime_data/src/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: import.meta.env.VITE_API_BASE_URL, //"http://localhost:3000/api", //import.meta.env.VITE_API_BASE_URL,//"https://or-ecosystem.eu/api",
-	//baseURL: "http://localhost:3000/api",
-	withCredentials: true,
+  baseURL: "https://or-ecosystem.eu/api", //"http://localhost:3000/api", //import.meta.env.VITE_API_BASE_URL,//"https://or-ecosystem.eu/api",
+  //baseURL: "http://localhost:3000/api",
+  withCredentials: true,
 });
 
 export default api;
