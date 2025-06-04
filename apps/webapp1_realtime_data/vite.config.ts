087768e721a36ce219c31344eb9e4bf3c98894f4
carluka <carluka33@gmail.com>
2025-06-04 15:40:29 +0200

    popravljen port

--- apps/webapp1_realtime_data/vite.config.ts
+++ apps/webapp1_realtime_data/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
+    port: 5173,
     host: true,
     allowedHosts: ["or-ecosystem.eu", "data.or-ecosystem.eu"],
   },
